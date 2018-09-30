c6fcc91f7e6833f4a4175bc7d26424df5fd6672d
humanjboof <humanjboof@gmail.com>
2018-09-30 15:20:27 -0400

    commented out examples/ ready to create database

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,34 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+// Everything is commented out because we need to create our own database and figure out what routes we are using...
+// the commented out stuff is the basic set up we need but it will be easier once we get the database named and set up
+const routes = (app) => {
+  //   // Load index page
+  //   app.get("/", function(req, res) {
+  //     db.Example.findAll({}).then(function(dbExamples) {
+  //       res.render("index", {
+  //         msg: "Welcome!",
+  //         examples: dbExamples
+  //       });
+  //     });
+  //   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  //   // Load example page and pass in an example by id
+  //   app.get("/example/:id", function(req, res) {
+  //     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //       res.render("example", {
+  //         example: dbExample
+  //       });
+  //     });
+  //   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  //   // Render 404 page for any unmatched routes
+  //   app.get("*", function(req, res) {
+  //     res.render("404");
+  //   });
+
 };
+
+module.exports = routes;
+
+
