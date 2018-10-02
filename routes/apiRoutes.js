06d3f8d650e9887be6d1c0fb7ce818a00addeade
humanjboof <humanjboof@gmail.com>
2018-10-02 17:59:59 -0400

    logging out the data from the api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,6 @@
-var db = require("../models");
+const db = require("../models");
+const podSearch = require('podcast-search');
 
-// Everything is commented out because we need to create our own database and figure out what routes we are using...
-// the commented out stuff is the basic set up we need but it will be easier once we get the database named and set up
 const routes = (app) => {
   app.get('/', (req, res) => {
     res.render('index')
@@ -10,8 +9,23 @@ const routes = (app) => {
   app.post('/search/term', (req, res) => {
     let searches = req.body.term
     console.log(searches);
+    getPod(searches)
   })
 
 };
+
+//getting data back in the console... this is just URL links to the podcast but we can also pull down title, image, logo, descriptions 
+//and whatever else we need just gotta render it to handlebars
+const getPod = (searchTerm) => {
+  let dataArray = []
+  podSearch.search(searchTerm)
+    .then(data => {
+      data.forEach(descript => {
+        dataArray.push({ data: descript.url });
+      })
+      console.log(dataArray)
+      return dataArray;
+    })
+}
 
 module.exports = routes;
