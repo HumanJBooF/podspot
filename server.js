578cd01869c2e8af9cd30b10644ff180d6b005ac
humanjboof <humanjboof@gmail.com>
2018-10-03 15:56:49 -0400

    object ready for handlebars

--- server.js
+++ server.js
@@ -16,12 +16,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Routes
